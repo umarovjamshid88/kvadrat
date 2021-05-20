50e0836eb3145bff852db962cfb89bb10398249f
umarovjamshid88 <58763851+umarovjamshid88@users.noreply.github.com>
2021-05-20 12:05:56 +0500

    kvadrat

---
+++ script.js
@@ -0,0 +1,16 @@
+var a = +prompt('enter number');
+var b = +prompt('enter divided number');
+for (let i = 1; i < b; i++) {    
+    b = 1/b;
+}
+console.log(Math.fround(a**b));
+
+
+
+// let power = +prompt('enter exponent');
+// let num = +prompt('enter number')
+// let answer = 1;
+// for (let i = 0; i < power; i++) {
+//    answer = answer * num;
+// }
+// console.log(answer);
